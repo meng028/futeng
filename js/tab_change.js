27933a2725362aa074413c6ecfa8dbf4451c44ce
meng028 <15701664107@163.com>
2018-08-24 19:18:01 +0800

    关于富腾页面-

--- js/tab_change.js
+++ js/tab_change.js
@@ -1,16 +1,21 @@
 
 // hide all
-$(".section_content div").hide();
+$(".section_content_div").hide();
 $(".section_table dl").click(function(){
     $(".section_table dl").eq($(this).index()).addClass("active").siblings().removeClass("active");
-    $(".section_content div").hide().eq($(this).index()).show();
+    $(".section_content_div").hide().eq($(this).index()).show();
 });
 
 // show first
-$(".section_right div").hide().eq(0).show()
+$(".section_right_container").hide().eq(0).show();
+$(".section_table li:first").addClass("active");
 $(".section_table li").click(function(){
     $(".section_table li").eq($(this).index()).addClass("active").siblings().removeClass("active");
-    $(".section_right div").hide().eq($(this).index()).show();
+    $(".section_right_container").hide().eq($(this).index()).show();
 });
 
 
+$('.tab_list li').click(function () {
+    console.log($(this).index());
+    $(".tab_list li a").eq($(this).index()).addClass("active_").siblings().removeClass("active_");
+});
