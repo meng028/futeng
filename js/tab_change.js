8f6114d48ca9ecd063ab0c34acad2c794728f297
xiannvrong <1796940658@qq.com>
2018-08-28 16:45:21 +0800

    地产业——项目合作

--- js/tab_change.js
+++ js/tab_change.js
@@ -28,3 +28,13 @@ function numFun(){
     $(".tab-box>div").eq(_index).show().siblings().hide(); // 让内容框的第 _index 个显示出来，其他的被隐藏
     $(".tab-menu li").eq(_index).addClass("change").siblings().removeClass("change"); // 改变选中时候的选项框的样式，移除其他几个选项的样式
 }
+
+$(".submit_message").click(function(){
+    if($(".fill_in input").val() =="" && $(".fill_in textarea").val() =="") {
+        alert("请输入留言内容")
+    }else {
+        alert("提交成功");
+        $(".fill_in input").val("");
+        $(".fill_in textarea").val("");
+    }
+});
