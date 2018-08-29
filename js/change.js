67ce100617b154594ebc182f4b6e71a9bb7f1cca
meng028 <15701664107@163.com>
2018-08-29 14:01:55 +0800

    footer 完善

--- js/change.js
+++ js/change.js
@@ -3,7 +3,16 @@
 // hide all
 $(".section_content_item").hide();
 $(".section_table .content").click(function(){
+
+    $(".tab-menu").removeClass("active_tab");
+    $(".tab-box").removeClass("active_box");
+    // 底部tab添加样式
     $(".section_table .content").eq($(this).index()).addClass("home_bgm").siblings().removeClass("home_bgm");
+    // 上面对应的选项卡添加样式
+    $(".tab-menu").eq($(this).index()).addClass("active_tab").siblings().removeClass("active_tab");
+    // 内容
+    $(".tab-box").eq($(this).index()).addClass("active_box").siblings().removeClass("active_box");
+
     $(".section_content_item").hide().eq($(this).index()).show();
 });
 
